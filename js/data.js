d69594fd7d74d4166a7e08939050d40dcf8d966c
PetersonAjtakSom <47882869+PetersonAjtakSom@users.noreply.github.com>
2019-04-04 18:12:41 +0200

    Třetí verze

---
+++ js/data.js
@@ -0,0 +1,13 @@
+
+var krestan=[
+    {
+      'text':'Jako celek je křesťanství nejrozšířenějším světovým náboženstvím s více než 2 miliardami věřících, v oblasti náboženskéhovyznání silně převažuje na třech kontinentech <b>(v Evropě, Austrálii a Americe)</b>.Křesťanství se spolu s dalšími činiteli výrazně podílelo na formování tzv. západní civilizace'
+    }
+  ]
+ var bible = [
+    {
+      'text':'Bible je pro křesťany Písmo svaté, inspirované slovo o Božím jednání se světem v Ježíši Kristu, který je prvním zdrojem a poslední normou jejich učení i života.'
+    }
+  ]
+
+
