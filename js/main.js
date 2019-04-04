930db0161a2df5e7bacf679ace08f2686b02194c
PetersonAjtakSom <47882869+PetersonAjtakSom@users.noreply.github.com>
2019-04-04 18:12:58 +0200

    Třetí verze

--- js/main.js
+++ js/main.js
@@ -1,6 +1,25 @@
+
+$(' h4').css({'border-bottom':'1px solid black'})
+.addClass('mt-4 p-3 bg-info');
+
+$('#krest h4').css({'border-bottom':'1px solid black'})
+.addClass('mt-4 p-3 bg-warning');
+
+$('#hind h4').css({'border-bottom':'1px solid black'})
+.addClass('mt-4 p-3 bg-warning');
+
+$('#budha h4').css({'border-bottom':'1px solid black'})
+.addClass('mt-4 p-3 bg-warning');
+
+$('#sikh h4').css({'border-bottom':'1px solid black'})
+.addClass('mt-4 p-3 bg-warning');
+
+$('#dzin h4').css({'border-bottom':'1px solid black'})
+.addClass('mt-4 p-3 bg-warning');
+
+
+
 $(function(){
-    //document.getElementsByTagName('h1')[0].style.display = 'none';
-    /* Změny zobrazení v sekci ustava */
     $('#odkazy h4').click(function(){
         if ($(this).nextUntil('h4').is(':hidden')) {
             $(this).css({'background-color':'#900'});
@@ -9,5 +28,19 @@ $(function(){
         }
         $(this).nextUntil('h4').toggle(1000);
     });
+});
 
-})
+$(function(){
+    $('#odkazy h4').on('click',function(){
+        $('#rozdeleni').text(krestan.text)
+     });
+});
+
+$(function(){
+    $('#odkazy h4').on('click',function(){
+        $('#bible').text(krestan.text)
+     });
+});
+
+
+
